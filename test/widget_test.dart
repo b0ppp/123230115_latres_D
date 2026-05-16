3b44d4399df27fe7070cb0290647b1b1d1ebd52b
b0ppp <kalladararaja84@gmail.com>
2026-05-16 14:35:09 +0700

    update minor

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:resep_masakan/main.dart';
+import 'package:toko_online/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
